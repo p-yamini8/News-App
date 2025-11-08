ede67b526411cf485770355f351ad571851bc25d
p-yamini8 <yaminifreez1@gmail.com>
2025-11-08 13:34:15 +0530

    added signup & login

---
+++ routes/user.js
@@ -0,0 +1,6 @@
+const express=require('express');
+const userControllers=require('../controllers/user');
+const router=express.Router();
+router.post('/signup',userControllers.userSignup);
+router.post('/login',userControllers.userLogin);
+module.exports=router;
